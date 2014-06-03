25117178333981dc8fde28ac21c40793d9f50144
Artem Kostiuk <postatum@gmail.com>
2014-06-03 21:05:53 +0600

    Trying to include docs

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Forcing developers to write docstrings.',
     author='Artem Kostiuk',
     author_email='postatum@gmail.com',
-    long_description='Forcing developers to write docstrings.',
+    long_description=open('README.md', 'r').read(),
     url='https://github.com/postatum/django-wtfd',
     download_url = 'https://github.com/postatum/django-wtfd/tarball/0.1.2',
     packages=[
